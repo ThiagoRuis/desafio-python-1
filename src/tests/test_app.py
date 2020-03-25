8199779749ab29ed26daf7e5473549b41357571f
John Doe <thiagoruis.mail@gmail.com>
2020-03-25 18:31:08 -0300

    Adicionado teste para função q computa o tempo

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,6 +1,7 @@
 import pytest, json
 
 from controller import *
+from utils import compute_time, compute_skills
 
 class TestApp:
     def test_empty_entry(self, client):
@@ -18,7 +19,7 @@ class TestApp:
             'Content-Type': mimetype,
             'Accept': mimetype
         }
-        with open('../examples/freelancer_no_skills.json') as json_file:
+        with open('./examples/freelancer_no_skills.json') as json_file:
             data = json.load(json_file)
             res = client.post('/freelance', json=dict(data), headers=headers)
             assert res.status_code == 400
@@ -30,7 +31,7 @@ class TestApp:
             'Content-Type': mimetype,
             'Accept': mimetype
         }
-        with open('../examples/freelancer_experience_single_no_interval.json') as json_request:
+        with open('./examples/freelancer_experience_single_no_interval.json') as json_request:
             data = json.load(json_request)
             res = client.post('/freelance', json=dict(data), headers=headers)
             assert res.status_code == 200
@@ -49,7 +50,7 @@ class TestApp:
             'Content-Type': mimetype,
             'Accept': mimetype
         }
-        with open('../examples/freelancer.json') as json_request:
+        with open('./examples/freelancer.json') as json_request:
             data = json.load(json_request)
             res = client.post('/freelance', json=dict(data), headers=headers)
             assert res.status_code == 200
@@ -62,3 +63,24 @@ class TestApp:
                 {'durationInMonths': 40, 'id': '400', 'name': 'Java'}
             ]
             assert computed_skills == expected_skills
+
+    def test_compute_time(self, client):
+        first_interval = {
+            "start": "2016-01-01",
+            "end": "2016-05-01"
+        }
+
+        buffer = compute_time(first_interval['start'], first_interval['end'], [])
+        expected_response = ['2016-01', '2016-02', '2016-03', '2016-04']
+        assert sorted(buffer) == (expected_response)
+        assert len(buffer) == 4
+
+        second_interval = {
+            "start": "2018-01-01",
+            "end": "2018-05-01"
+        }
+
+        response = compute_time(second_interval['start'], second_interval['end'], buffer)
+        expected_response = ['2016-01', '2016-02', '2016-03', '2016-04', '2018-01', '2018-02', '2018-03', '2018-04']
+        assert sorted(response) == (expected_response)
+        assert len(response) == 8
